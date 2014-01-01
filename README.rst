a87ff75ced03aa103c8ac5cd791d6082f3584924
David A. Riggs <driggs@myotisoft.com>
2014-01-01 10:55:35 -0500

    GitHub and doc updates to project

--- README.rst
+++ README.rst
@@ -59,4 +59,19 @@ Botswana, which features a remote cave housing a large python-shaped rock which 
 70,000 years.
 
 
+Installation
+------------
+
+Releases are available for installation from the Python Package Index, see `installation instructions <https://wiki.python.org/moin/CheeseShopTutorial#Installing_Distributions>`_.
+
+``pip install tsodilo``
+
+Or, if you've downloaded a source distribution or checked out from the git repository, install locally with:
+
+``python setup.py install``
+
+
+License
+-------
+
 Tsodilo is Open Source software licensed under the MIT License, and is copyright (C) Myotisoft LLC.
--- setup.py
+++ setup.py
@@ -2,23 +2,23 @@
 
 from distutils.core import setup
 
-LONG_DESCRIPTION = open('README.txt').read()
+LONG_DESCRIPTION = open('README.rst').read()
 
 setup(
 	name='tsodilo',
 	version='0.0.1',
 	author='David A. Riggs',
 	author_email='driggs@myotisoft.com',
-	url='http://myotisoft.com',
+	url='https://github.com/riggsd/tsodilo',
 	description='Package for manipulating cave survey data',
 	long_description=LONG_DESCRIPTION,
 	packages=['tsodilo'],
-    keywords=['cave', 'survey', 'gis'],
+	keywords=['cave', 'survey', 'gis'],
 	classifiers=[
 		'Development Status :: 2 - Pre-Alpha',
 		'License :: OSI Approved :: MIT License',
-	    'Programming Language :: Python :: 2.7',
-	    'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2.7',
+		'Operating System :: OS Independent',
 		'Intended Audience :: Science/Research',
 		'Intended Audience :: Developers',
 		'Topic :: Scientific/Engineering :: GIS',
