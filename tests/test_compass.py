e2a292b0f567b98d46608cc179868a6ab7c57f17
David A. Riggs <driggs@myotisoft.com>
2014-01-13 14:24:11 -0500

    PEP8 source cleanups

--- tests/test_compass.py
+++ tests/test_compass.py
@@ -14,30 +14,30 @@ TESTFILE = 'tests/data/compass/FULFORDS.MAK'
 
 
 class CompassParsingTestCase(unittest.TestCase):
-	"""In liu of a real test suite, just drive the code by parsing a known file"""
+    """In liu of a real test suite, just drive the code by parsing a known file"""
 
-	def runTest(self):
-		makparser = compass.CompassProjectParser(TESTFILE)
+    def runTest(self):
+        makparser = compass.CompassProjectParser(TESTFILE)
 
-		project = makparser.parse()
-		self.assertEqual(project.name, 'FULFORDS')
-		self.assertEqual(len(project), 2)
+        project = makparser.parse()
+        self.assertEqual(project.name, 'FULFORDS')
+        self.assertEqual(len(project), 2)
 
-		cave_survey_dat = project.linked_files[0]
-		self.assertEqual(cave_survey_dat.name, 'FULFORD')
-		self.assertEqual(len(cave_survey_dat), 25)
-		self.assertTrue('BS' in cave_survey_dat)
+        cave_survey_dat = project.linked_files[0]
+        self.assertEqual(cave_survey_dat.name, 'FULFORD')
+        self.assertEqual(len(cave_survey_dat), 25)
+        self.assertTrue('BS' in cave_survey_dat)
 
-		bs_survey = cave_survey_dat['BS']
-		self.assertTrue('Stan Allison' in bs_survey.team)
-		self.assertEqual(bs_survey.date, datetime.date(1989, 2, 11))
-		#self.assertEqual(bs_survey.declination, 11.18)  # TODO: implement declination
-		self.assertEqual(len(bs_survey), 15)
+        bs_survey = cave_survey_dat['BS']
+        self.assertTrue('Stan Allison' in bs_survey.team)
+        self.assertEqual(bs_survey.date, datetime.date(1989, 2, 11))
+        #self.assertEqual(bs_survey.declination, 11.18)  # TODO: implement declination
+        self.assertEqual(len(bs_survey), 15)
 
-		last_shot = bs_survey.shots[-1]
-		self.assertEqual(last_shot['FROM'], 'BSA2')
-		self.assertEqual(last_shot['TO'], 'BS1')
-		self.assertEqual(last_shot['LENGTH'], 37.85)
-		self.assertEqual(last_shot['BEARING'], 307.0)
-		self.assertEqual(last_shot['INC'], -23.0)
-		self.assertEqual(last_shot['LEFT'], float('inf'))
+        last_shot = bs_survey.shots[-1]
+        self.assertEqual(last_shot['FROM'], 'BSA2')
+        self.assertEqual(last_shot['TO'], 'BS1')
+        self.assertEqual(last_shot['LENGTH'], 37.85)
+        self.assertEqual(last_shot['BEARING'], 307.0)
+        self.assertEqual(last_shot['INC'], -23.0)
+        self.assertEqual(last_shot['LEFT'], float('inf'))
